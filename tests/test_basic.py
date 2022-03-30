f497a45f12dd147c7c0177ccac3d713d88a2c7fc
crcj <cr.cj@outlook.com>
2022-03-30 05:17:22 -0300

    simple test with pytest

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,26 +1,35 @@
 import os
+import pytest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import finlogic as fi
 
 DATA_PATH = os.path.dirname(__file__)+ '/data/'
 
-# Petro indicators (consolidated)
-co = fi.Company(9512, acc_unit='billion')
-df_expected = pd.read_pickle(DATA_PATH + 'petr_ind_con.pkl')
-assert_frame_equal(co.indicators(), df_expected)
+def test_petro_indicators_consolidated():
+    # Petro indicators (consolidated)
+    co = fi.Company(9512, acc_unit='billion')
+    result = co.indicators()
+    expected = pd.read_pickle(DATA_PATH + 'petr_ind_con.pkl')
+    assert_frame_equal(expected, result)
 
-# Petro indicators (separate)
-co.acc_method = 'separate'
-df_expected = pd.read_pickle(DATA_PATH + 'petr_ind_sep.pkl')
-assert_frame_equal(co.indicators(), df_expected)
+def test_petro_indicators_separate():
+    # Petro indicators (separate)
+    co = fi.Company(9512, acc_unit='billion', acc_method='separate')
+    result = co.indicators()
+    expected = pd.read_pickle(DATA_PATH + 'petr_ind_sep.pkl')
+    assert_frame_equal(result, expected)
 
-# Agro indicators (consolidated)
-co = fi.Company(20036, acc_unit='million')
-df_expected = pd.read_pickle(DATA_PATH + 'agro_ind_con.pkl')
-assert_frame_equal(co.indicators(), df_expected)
+def test_agro_indicators_consolidated():
+    # Agro indicators (consolidated)
+    co = fi.Company(20036, acc_unit='million')
+    result = co.indicators()
+    expected = pd.read_pickle(DATA_PATH + 'agro_ind_con.pkl')
+    assert_frame_equal(result, expected)
 
-# Agro indicators (separate)
-co.acc_method = 'separate'
-df_expected = pd.read_pickle(DATA_PATH + 'agro_ind_sep.pkl')
-assert_frame_equal(co.indicators(), df_expected)
+def test_agro_indicators_separate():
+    # Agro indicators (separate)
+    co = fi.Company(20036, acc_unit='million', acc_method='separate')
+    result = co.indicators()
+    expected = pd.read_pickle(DATA_PATH + 'agro_ind_sep.pkl')
+    assert_frame_equal(result, expected)
