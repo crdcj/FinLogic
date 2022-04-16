72d18d89571e9bbae83161f1f8ae8d6b1e98e7e7
Carlos Carvalho <cr.cj@outlook.com>
2022-04-16 19:52:06 -0300

    updated pytest

--- tests/test_company.py
+++ tests/test_company.py
@@ -3,33 +3,32 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 from finlogic import Company
 
-RESULTS_PATH = os.path.dirname(__file__) + '/results/'
-print(RESULTS_PATH)
+RESULTS_PATH = os.path.dirname(__file__) + "/results/"
+
 
 def test_petro_indicators_consolidated():
     # Petro indicators (consolidated)
-    co = Company(9512, acc_unit='billion')
-    result = co.indicators()
-    expected = pd.read_pickle(RESULTS_PATH + 'petr_ind_con.pkl')
+    result = Company(9512, acc_unit="billion").indicators()
+    expected = pd.read_pickle(RESULTS_PATH + "petr_ind_con.pkl")
     assert_frame_equal(expected, result)
+
 
 def test_petro_indicators_separate():
     # Petro indicators (separate)
-    co = Company(9512, acc_unit='billion', acc_method='separate')
-    result = co.indicators()
-    expected = pd.read_pickle(RESULTS_PATH + 'petr_ind_sep.pkl')
+    result = Company(9512, acc_unit="billion", acc_method="separate").indicators()
+    expected = pd.read_pickle(RESULTS_PATH + "petr_ind_sep.pkl")
     assert_frame_equal(result, expected)
+
 
 def test_agro_indicators_consolidated():
     # Agro indicators (consolidated)
-    co = Company(20036, acc_unit='million')
-    result = co.indicators()
-    expected = pd.read_pickle(RESULTS_PATH + 'agro_ind_con.pkl')
+    result = Company(20036, acc_unit="million").indicators()
+    expected = pd.read_pickle(RESULTS_PATH + "agro_ind_con.pkl")
     assert_frame_equal(result, expected)
+
 
 def test_agro_indicators_separate():
     # Agro indicators (separate)
-    co = Company(20036, acc_unit='million', acc_method='separate')
-    result = co.indicators()
-    expected = pd.read_pickle(RESULTS_PATH + 'agro_ind_sep.pkl')
+    result = Company(20036, acc_unit="million", acc_method="separate").indicators()
+    expected = pd.read_pickle(RESULTS_PATH + "agro_ind_sep.pkl")
     assert_frame_equal(result, expected)
