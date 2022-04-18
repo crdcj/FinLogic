d2cf61e7b641aa8a32d563a92419b92e6c330ce8
Carlos Carvalho <cr.cj@outlook.com>
2022-04-18 09:54:17 -0300

    new version

--- finlogic/__init__.py
+++ finlogic/__init__.py
@@ -6,10 +6,12 @@
 # Copyright 2022 Carlos Carvalho
 #
 from . import config  # noqa
-from .version import __version__  # noqa
+from . import version
 from .database import update_database, search_company, database_info
 from .company import Company
 
+
+__version__ = version.version
 __author__ = "Carlos Carvalho"
 
 __all__ = [
