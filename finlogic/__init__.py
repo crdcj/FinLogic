fb3d7eee318581268684686a7de6c73ed922e49f
crcj <cr.cj@outlook.com>
2023-04-02 09:46:06 -0300

    version update in PyPI

--- finlogic/__init__.py
+++ finlogic/__init__.py
@@ -10,7 +10,7 @@ from .database import update_database, search_company, database_info
 from .company import Company
 
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Carlos Carvalho"
 
 __all__ = [
