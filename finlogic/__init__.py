e51996f1cafaf5583de24fb1f75736ea81326539
crcj <cr.cj@outlook.com>
2023-04-09 17:50:57 -0300

    new version

--- finlogic/__init__.py
+++ finlogic/__init__.py
@@ -10,7 +10,7 @@ from .database import update_database, search_company, database_info
 from .company import Company
 
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 __author__ = "Carlos Carvalho"
 
 __all__ = [
