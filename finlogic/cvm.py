eec524230be260bdb2b6b0dac7015610c4df6ffb
crcj <cr.cj@outlook.com>
2023-05-09 08:57:16 -0300

    wip

--- finlogic/cvm.py
+++ finlogic/cvm.py
@@ -55,18 +55,15 @@ def update_cvm_file(url: str, s) -> str:
     filename = url[-23:]  # filename = end of url
     filepath = Path(CVM_DIR, filename)
     headers = s.head(url).headers
-    # Get the filesize from the local file
     filesize = filepath.stat().st_size if filepath.exists() else 0
-    # Compare the filesize with the Content-Length header
     if filesize == int(headers["Content-Length"]):
-        # File is the same, no need to update it
-        print(f"    - {filename} already updated.")
+        print(f"    - {filename} is the same -> skip.")
         return None
     r = s.get(url)
     if r.status_code != 200:
         return None
 
-    # Save the file with Pathlib
+    # Save file with Pathlib
     filepath.write_bytes(r.content)
     print(f"    {CHECKMARK} {filename} updated.")
 
@@ -84,55 +81,30 @@ def update_cvm_files(urls: str) -> List[str]:
     return updated_filenames
 
 
-def read_child_file(child_file: str) -> pd.DataFrame:
-    """Read a child file, process it and return a dataframe."""
-    # Only "DT_INI_EXERC" and "COLUNA_DF" have missing values.
-    df = pd.read_csv(
-        child_file,
-        sep=";",
-        encoding="iso-8859-1",
-        true_values=["S"],
-        false_values=["N"],
-    )
-    # Currency column has only one value (BRL) so it is not necessary.
-    df = df.drop(columns=["MOEDA"])
-    return df
-
-
-def read_cvm_file(cvm_filename: str) -> pd.DataFrame:
+def read_cvm_file(cvm_filepath: Path) -> pd.DataFrame:
     """Read annual file, process it, save the result and return the file path."""
-    df = pd.DataFrame()
-    cvm_filepath = Path(CVM_DIR, cvm_filename)
-    annual_zipfile = zf.ZipFile(cvm_filepath)
-    child_filenames = annual_zipfile.namelist()
+    cvm_zipfile = zf.ZipFile(cvm_filepath)
+    child_filenames = cvm_zipfile.namelist()
+
+    # Filename example for the first file in zip: "dfp_cia_aberta_2022.csv"
+    # Do not read the first file, since it is a metadata file.
+    child_filenames = child_filenames[1:]
 
     df_list = []
-    for child_filename in child_filenames[1:]:
-        child_file = annual_zipfile.open(child_filename)
-        child_df = read_child_file(child_file)
+    for child_filename in child_filenames:
+        child_zf = cvm_zipfile.open(child_filename)
+        child_df = pd.read_csv(child_zf, sep=";", encoding="iso-8859-1")
         df_list.append(child_df)
     df = pd.concat(df_list, ignore_index=True)
-
-    # There are two types of CVM files: DFP (ANNUAL) and ITR (QUARTERLY).
-    if cvm_filepath.name.startswith("dfp"):
-        child_df["TIPO"] = "ANNUAL"
-    else:
-        child_df["TIPO"] = "QUARTERLY"
-
-    df["ARQUIVO"] = cvm_filepath.name
-    # Convert string columns to categorical.
-    columns = df.select_dtypes(include="object").columns
-    df[columns] = df[columns].astype("category")
     return df
 
 
-def format_cvm_df(df: pd.DataFrame) -> pd.DataFrame:
+def format_cvm_df(df: pd.DataFrame, filepath: Path) -> pd.DataFrame:
     """Format a cvm dataframe."""
     columns_translation = {
         "DENOM_CIA": "co_name",
         "CD_CVM": "co_id",
         "CNPJ_CIA": "co_fiscal_id",
-        "TIPO": "report_type",
         "VERSAO": "report_version",
         "DT_REFER": "period_reference",
         "DT_INI_EXERC": "period_begin",
@@ -143,16 +115,41 @@ def format_cvm_df(df: pd.DataFrame) -> pd.DataFrame:
         "ST_CONTA_FIXA": "acc_fixed",
         "VL_CONTA": "acc_value",
         "COLUNA_DF": "equity_statement_column",
-        "ARQUIVO": "data_source",
         # Columns below will be dropped after processing.
         "GRUPO_DFP": "report_group",
+        "MOEDA": "Currency",
         "ESCALA_MOEDA": "currency_unit",
     }
     df = df.rename(columns=columns_translation)[columns_translation.values()]
 
+    # Currency column has only one value (BRL) so it is not necessary.
+    df = df.drop(columns=["Currency"])
+
+    # There are two types of CVM files: DFP (ANNUAL) and ITR (QUARTERLY).
+    # "report_type" should be positioned after "co_fiscal_id" -> position = 3.
+    if filepath.name.startswith("dfp"):
+        df.insert(loc=3, column="report_type", value="ANNUAL")
+    else:
+        df.insert(loc=3, column="report_type", value="QUARTERLY")
+
+    df["data_source"] = filepath.name
+
+    # The are double spaces in co_name, acc_name and equity_statement_column.
+    columns = ["co_name", "acc_name", "equity_statement_column"]
+    df[columns] = df[columns].apply(lambda x: x.str.replace("  ", " "))
+    # Strip leading and trailing spaces from those columns.
+    df[columns] = df[columns].apply(lambda x: x.str.strip())
+
+    # Convert string columns to categorical before mapping.
+    columns = df.select_dtypes(include="object").columns
+    df[columns] = df[columns].astype("category")
+
+    # "acc_fixed" values are: 'S', 'N'
+    map_dic = {"S": True, "N": False}
+    df["acc_fixed"] = df["acc_fixed"].map(map_dic).astype(bool)
     # currency_unit values are ['MIL', 'UNIDADE']
-    map_dict = {"UNIDADE": 1, "MIL": 1000}
-    df["currency_unit"] = df["currency_unit"].map(map_dict).astype(int)
+    map_dic = {"UNIDADE": 1, "MIL": 1000}
+    df["currency_unit"] = df["currency_unit"].map(map_dic).astype(int)
 
     # Do not ajust acc_value for 3.99 codes.
     df["acc_value"] = df["acc_value"].where(
@@ -162,8 +159,8 @@ def format_cvm_df(df: pd.DataFrame) -> pd.DataFrame:
     df.drop(columns=["currency_unit"], inplace=True)
 
     # "period_order" values are: 'ÚLTIMO', 'PENÚLTIMO'
-    map_dict = {"ÚLTIMO": "LAST", "PENÚLTIMO": "PREVIOUS"}
-    df["period_order"] = df["period_order"].map(map_dict)
+    map_dic = {"ÚLTIMO": "LAST", "PENÚLTIMO": "PREVIOUS"}
+    df["period_order"] = df["period_order"].map(map_dic)
     """
     acc_method -> Financial Statemen Type
     Consolidated and Separate Financial Statements (IAS 27/2003)
@@ -186,22 +183,26 @@ def format_cvm_df(df: pd.DataFrame) -> pd.DataFrame:
     if == 'Con' -> consolidated statement
     if == 'Ind' -> separate statement
     """
-    map_dict = {"Con": "CONSOLIDATED", "Ind": "SEPARATE"}
-    df.insert(9, "acc_method", df["report_group"].str[3:6].map(map_dict))
+    map_dic = {"Con": "CONSOLIDATED", "Ind": "SEPARATE"}
+    df.insert(9, "acc_method", df["report_group"].str[3:6].map(map_dic))
     # 'GRUPO_DFP' data can be inferred from 'acc_code'
     df.drop(columns=["report_group"], inplace=True)
+
     # Correct/harmonize some account texts.
-    df["acc_name"].replace(to_replace=["\xa0ON\xa0", "On"], value="ON", inplace=True)
-    # From 20_636_037 rows 2_383 were duplicated on 2023-04-30 -> remove duplicates
+    # df["acc_name"].replace(to_replace=["\xa0ON\xa0", "On"], value="ON", inplace=True)
+
+    # For example, in "itr_cia_aberta_2022.zip" 2_742 rows are duplicated.
+    # Few of them have different values in "acc_value". MARK THOSE ROWS AS ERRORS?
     cols = df.columns.tolist()
     cols.remove("acc_value")
-    df.drop_duplicates(subset=cols, keep="last", inplace=True)
+    df.drop_duplicates(subset=cols, keep="last", inplace=True, ignore_index=True)
 
     return df
 
 
 def process_cvm_file(cvm_filename: str) -> pd.DataFrame:
     """Read and format a CVM file."""
-    df = read_cvm_file(cvm_filename)
-    df = format_cvm_df(df)
+    cvm_filepath = Path(CVM_DIR, cvm_filename)
+    df = read_cvm_file(cvm_filepath)
+    df = format_cvm_df(df, cvm_filepath)
     return df
