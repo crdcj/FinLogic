572eedacf6618a4a84f97271b733a241de333be7
crcj <cr.cj@outlook.com>
2023-06-01 13:32:31 -0300

    wip

--- finlogic/indicators.py
+++ finlogic/indicators.py
@@ -5,16 +5,25 @@ TAX_RATE = 0.34
 
 
 def build_indicators() -> pd.DataFrame:
-    codes_dict = {
+    indicators_codes = {
+        "1": "total_assets",
+        "1.01": "current_assets",
         "1.01.01": "cash_equivalents",
         "1.01.02": "financial_investments",
+        "2.01": "current_liabilities",
         "2.01.04": "short_term_debt",
         "2.02.01": "long_term_debt",
         "2.03": "equity",
         "3.01": "revenues",
+        "3.03": "gross_profit",
         "3.05": "ebit",
+        "3.07": "ebt",
+        "3.08": "effective_tax",
+        "3.11": "net_income",
+        "6.01": "operating_cash_flow",
+        "6.01.01.04": "depreciation_amortization",
     }
-    codes = list(codes_dict.keys())  # noqa: used in query below
+    codes = list(indicators_codes.keys())  # noqa: used in query below
     df = (
         dm.get_main_df()
         .query("acc_code in @codes and not is_annual")
@@ -31,32 +40,59 @@ def build_indicators() -> pd.DataFrame:
         columns=["acc_code"],
     ).reset_index()
 
-    dfp.rename(columns=codes_dict, inplace=True)
+    dfp.rename(columns=indicators_codes, inplace=True)
+
+    dfp["working_capital"] = dfp["current_assets"] - dfp["current_liabilities"]
 
     dfp["total_cash"] = dfp["cash_equivalents"] + dfp["financial_investments"]
     dfp.drop(columns=["cash_equivalents", "financial_investments"], inplace=True)
 
     dfp["total_debt"] = dfp["short_term_debt"] + dfp["long_term_debt"]
+    dfp["net_debt"] = dfp["total_debt"] - dfp["total_cash"]
     dfp.drop(columns=["short_term_debt", "long_term_debt"], inplace=True)
 
+    dfp["effective_tax_rate"] = -1 * dfp["effective_tax"] / dfp["ebt"]
+
+    dfp["ebitda"] = dfp["ebit"] + dfp["depreciation_amortization"]
+
     dfp["invested_capital"] = dfp["total_debt"] + dfp["equity"] - dfp["total_cash"]
-    dfp.drop(columns=["total_debt", "equity", "total_cash"], inplace=True)
 
     gp_cols = ["cvm_id", "is_annual", "is_consolidated"]
-    dfp["ic_prev_4"] = dfp.groupby(by=gp_cols)["invested_capital"].shift(4)
-    dfp["ic_prev_1"] = dfp.groupby(by=gp_cols)["invested_capital"].shift(1)
+    dfp["invested_capital_p4"] = dfp.groupby(by=gp_cols)["invested_capital"].shift(4)
+    dfp["invested_capital_p1"] = dfp.groupby(by=gp_cols)["invested_capital"].shift(1)
 
-    dfp["ic_prev"] = dfp["ic_prev_4"]
-    dfp["ic_prev"].fillna(dfp["ic_prev_1"], inplace=True)
-    dfp["ic_prev"].fillna(dfp["invested_capital"], inplace=True)
+    dfp["invested_capital_p"] = dfp["invested_capital_p4"]
+    dfp["invested_capital_p"].fillna(dfp["invested_capital_p1"], inplace=True)
+    dfp["invested_capital_p"].fillna(dfp["invested_capital"], inplace=True)
 
-    dfp["average_invested_capital"] = (dfp["invested_capital"] + dfp["ic_prev"]) / 2
+    dfp["avg_invested_capital"] = (
+        dfp["invested_capital"] + dfp["invested_capital_p"]
+    ) / 2
+
+    dfp["equity_p4"] = dfp.groupby(by=gp_cols)["equity"].shift(4)
+    dfp["equity_p1"] = dfp.groupby(by=gp_cols)["equity"].shift(1)
+
+    dfp["equity_p"] = dfp["equity_p4"]
+    dfp["equity_p"].fillna(dfp["equity_p1"], inplace=True)
+    dfp["equity_p"].fillna(dfp["equity"], inplace=True)
+    dfp["avg_equity"] = (dfp["equity"] + dfp["equity_p"]) / 2
 
     # Drop temporary columns
-    dfp.drop(columns=["ic_prev", "ic_prev_1", "ic_prev_4"], inplace=True)
+    dfp.drop(
+        columns=[
+            "invested_capital_p",
+            "invested_capital_p1",
+            "invested_capital_p4",
+            "equity_p",
+            "equity_p1",
+            "equity_p4",
+        ],
+        inplace=True,
+    )
 
     dfp = dfp.groupby(by=gp_cols).tail(1).dropna().reset_index(drop=True)
 
-    dfp["roic"] = (1 - TAX_RATE) * dfp["ebit"] / dfp["average_invested_capital"]
+    dfp["roic"] = dfp["ebit"] * (1 - TAX_RATE) / dfp["avg_invested_capital"]
+    dfp["roe"] = dfp["ebit"] * (1 - TAX_RATE) / dfp["avg_equity"]
 
     return dfp
