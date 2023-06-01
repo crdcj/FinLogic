dba779b250ae27a5c8539d12f50ec18de35e4cf9
crcj <cr.cj@outlook.com>
2023-06-01 07:15:48 -0300

    wip

--- finlogic/indicators.py
+++ finlogic/indicators.py
@@ -1,11 +1,20 @@
 import pandas as pd
 from . import data_manager as dm
 
+TAX_RATE = 0.34
+
 
 def build_indicators() -> pd.DataFrame:
-    stock_codes = ["2.03", "1.01.01", "1.01.02", "2.01.04", "2.02.01"]
-    flow_codes = ["3.01", "3.05"]
-    codes = stock_codes + flow_codes  # noqa
+    codes_dict = {
+        "1.01.01": "cash_equivalents",
+        "1.01.02": "financial_investments",
+        "2.01.04": "short_term_debt",
+        "2.02.01": "long_term_debt",
+        "2.03": "equity",
+        "3.01": "revenues",
+        "3.05": "ebit",
+    }
+    codes = list(codes_dict.keys())  # noqa: used in query below
     df = (
         dm.get_main_df()
         .query("acc_code in @codes and not is_annual")
@@ -15,47 +24,39 @@ def build_indicators() -> pd.DataFrame:
     df["acc_code"] = df["acc_code"].astype("string")
     # df["period_begin"].fillna(df["period_end"], inplace=True)
 
-    index_cols = ["cvm_id", "is_annual", "is_consolidated", "period_end"]
-
-    dfs = pd.pivot_table(
-        df.query("acc_code in @stock_codes"),
+    dfp = pd.pivot_table(
+        df,
         values="acc_value",
-        index=index_cols,
+        index=["cvm_id", "is_annual", "is_consolidated", "period_end"],
         columns=["acc_code"],
     ).reset_index()
 
-    dfs["total_cash"] = dfs["1.01.01"] + dfs["1.01.02"]
-    dfs["total_debt"] = dfs["2.01.04"] + dfs["2.02.01"]
-    dfs.drop(columns=["1.01.01", "1.01.02", "2.01.04", "2.02.01"], inplace=True)
-    dfs["equity"] = dfs["2.03"]
-    dfs.drop(columns=["2.03"], inplace=True)
-    dfs["invested_capital"] = dfs["total_debt"] + dfs["equity"] - dfs["total_cash"]
-    dfs.drop(columns=["total_debt", "equity", "total_cash"], inplace=True)
+    dfp.rename(columns=codes_dict, inplace=True)
+
+    dfp["total_cash"] = dfp["cash_equivalents"] + dfp["financial_investments"]
+    dfp.drop(columns=["cash_equivalents", "financial_investments"], inplace=True)
+
+    dfp["total_debt"] = dfp["short_term_debt"] + dfp["long_term_debt"]
+    dfp.drop(columns=["short_term_debt", "long_term_debt"], inplace=True)
+
+    dfp["invested_capital"] = dfp["total_debt"] + dfp["equity"] - dfp["total_cash"]
+    dfp.drop(columns=["total_debt", "equity", "total_cash"], inplace=True)
 
     gp_cols = ["cvm_id", "is_annual", "is_consolidated"]
-    # Divide in annual and quarterly, because the shit is different
-    # Annual data will be shifted 1 year and quarterly 4 quarters
-    # Annual dataframe
+    dfp["ic_prev_4"] = dfp.groupby(by=gp_cols)["invested_capital"].shift(4)
+    dfp["ic_prev_1"] = dfp.groupby(by=gp_cols)["invested_capital"].shift(1)
 
-    dfs["ic_prev_4"] = dfs.groupby(by=gp_cols)["invested_capital"].shift(4)
-    dfs["ic_prev_1"] = dfs.groupby(by=gp_cols)["invested_capital"].shift(1)
-    dfs["ic_prev"] = dfs["ic_prev_4"]
-    dfs["ic_prev"].fillna(dfs["ic_prev_1"], inplace=True)
-    dfs["ic_prev"].fillna(dfs["invested_capital"], inplace=True)
-    dfs["average_invested_capital"] = (dfs["invested_capital"] + dfs["ic_prev"]) / 2
-    # Drop intermediary columns
-    dfs.drop(columns=["ic_prev", "ic_prev_1", "ic_prev_4"], inplace=True)
-    dfs = dfs.groupby(by=gp_cols).tail(1).dropna().reset_index(drop=True)
+    dfp["ic_prev"] = dfp["ic_prev_4"]
+    dfp["ic_prev"].fillna(dfp["ic_prev_1"], inplace=True)
+    dfp["ic_prev"].fillna(dfp["invested_capital"], inplace=True)
 
-    dff = pd.pivot_table(
-        df.query("acc_code in @flow_codes"),
-        values="acc_value",
-        index=index_cols,
-        columns=["acc_code"],
-    ).reset_index()
+    dfp["average_invested_capital"] = (dfp["invested_capital"] + dfp["ic_prev"]) / 2
 
-    dff = dff.groupby(by=gp_cols).tail(1)
-    # dff.drop()
-    on_cols = ["cvm_id", "is_annual", "is_consolidated", "period_end"]
-    df = dfs.merge(dff, how="inner", on=on_cols)
-    return df
+    # Drop temporary columns
+    dfp.drop(columns=["ic_prev", "ic_prev_1", "ic_prev_4"], inplace=True)
+
+    dfp = dfp.groupby(by=gp_cols).tail(1).dropna().reset_index(drop=True)
+
+    dfp["roic"] = (1 - TAX_RATE) * dfp["ebit"] / dfp["average_invested_capital"]
+
+    return dfp
