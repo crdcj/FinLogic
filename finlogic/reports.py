e391a9128cd685c33d5bec671b72c448202b2bfb
crcj <cr.cj@outlook.com>
2023-06-04 06:52:24 -0300

    wip

--- finlogic/reports.py
+++ finlogic/reports.py
@@ -7,8 +7,7 @@ def read_all_processed_files() -> pd.DataFrame:
     # list filepaths in processed folder
     filepaths = sorted(cfg.CVM_PROCESSED_DIR.glob("*.pickle"))
     df = pd.concat([pd.read_pickle(f, compression="zstd") for f in filepaths])
-    # expr = "cvm_id == 9512 and (acc_code == '3.01' or acc_code == '1')"
-    # df.query(expr=expr, inplace=True)
+    # df.query("cvm_id == 9512", inplace=True)
     return df.reset_index(drop=True)
 
 
@@ -44,7 +43,7 @@ def get_ltm_mask(dfi: pd.DataFrame) -> pd.Series:
     reports.
     Conditions that need to be met for each company:
         (1) Last quarter "period_reference" > last annual "period_reference"
-        (2) Last "period_reference" of annual reports
+        (2) Last "period_end" of annual reports
         (3) Last "period_reference" of quarterly reports
         (4) Only income and cash flow statements are adjusted to LTM
     """
@@ -61,7 +60,7 @@ def get_ltm_mask(dfi: pd.DataFrame) -> pd.Series:
 
     # Condition (2)
     mask1 = df["is_annual"]
-    mask2 = df["period_reference"] == df["last_annual"]
+    mask2 = df["period_end"] == df["last_annual"]
     cond2 = mask1 & mask2
 
     # Condition (3)
@@ -77,15 +76,15 @@ def get_ltm_mask(dfi: pd.DataFrame) -> pd.Series:
 
 def adjust_ltm(df: pd.DataFrame) -> pd.DataFrame:
     """Adjust income and cash flow statements to LTM (Last Twelve Months).
-    To get the LTM values, we     need to sum the current accumulated quarter
-    with the difference between the last annual value and the previous
-    accumulated quarter. Provided that quarterly values are always accumulated,
-    we can use the following formula:
-    LTM = current quarter + ( last annual - previous quarter)
+    To get the LTM values, we need to sum the current accumulated quarter with
+    the difference between the last annual value and the previous accumulated
+    quarter. Provided that quarterly values are always accumulated, we can use
+    the following formula:
+    LTM = current quarter + (last annual - previous quarter)
     Example for 1Q23: LTM = 1Q23 + (A22 - 1Q22)
     Example for 3Q23: LTM = 3Q23 + (A23 - 3Q23)
     """
-    # Quarters dataframe
+    # Get quarters dataframe
     quarters = df.query("not is_annual").reset_index(drop=True)
 
     # Get current quarter dataframe
@@ -93,12 +92,12 @@ def adjust_ltm(df: pd.DataFrame) -> pd.DataFrame:
     mask = quarters["period_end"] == grouped.transform("max")
     current_quarter = quarters[mask].reset_index(drop=True)
 
-    # Get previous quarter dataframe
+    # Get previous quarter dataframe and adjust values
     mask = quarters["period_end"] == grouped.transform("min")
     previous_quarter = quarters[mask].reset_index(drop=True)
     previous_quarter["acc_value"] = (-1) * previous_quarter["acc_value"]
 
-    # Annuals dataframe
+    # Get annuals dataframe
     annuals = df.query("is_annual").reset_index(drop=True)
 
     # Build LTM adjusted dataframe
@@ -110,7 +109,7 @@ def adjust_ltm(df: pd.DataFrame) -> pd.DataFrame:
         .sum()
         .reset_index()
     )
-    # current_quarter receives the LTM values
+    # current_quarter receives LTM values
     current_quarter.drop(columns="acc_value", inplace=True)
     ltm = pd.merge(current_quarter, ltm)
     ltm["period_begin"] = quarters["period_end"] - pd.DateOffset(years=1)
