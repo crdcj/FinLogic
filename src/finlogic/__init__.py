a64203551a9fdc524327432864d42fc79b3b9d35
Carlos Carvalho <carlos.carvalho.jr@gmail.com>
2026-05-14 01:25:11 -0300

    Refactor project structure and add core functionality for financial data management - Update .gitignore to exclude unnecessary files - Modify pyproject.toml to change build system and dependencies - Implement Company class for financial report generation - Add data management functions for loading and processing financial data - Introduce indicators module for financial metrics calculations

--- src/finlogic/__init__.py
+++ src/finlogic/__init__.py
@@ -3,12 +3,14 @@
 # FinLogic - Brazilian Company Financial Data
 # https://github.com/crdcj/FinLogic
 #
-# Copyright 2022 Carlos Carvalho
+# Copyright 2024 Carlos Carvalho
 #
+from importlib.metadata import version
+
 from .company import Company
 from .data import info, load, rank, search_company, search_segment
 
-__version__ = "0.6.3"
+__version__ = version("FinLogic")
 __author__ = "Carlos Carvalho"
 
 __all__ = ["Company", "load", "search_company", "info", "search_segment", "rank"]
